d56808121c9008b6483760ad17bbc78556582b1a
wxiea <43360560+wxiea@users.noreply.github.com>
2018-12-09 21:38:53 -0700

    Add files via upload

---
+++ Lab7.py
@@ -0,0 +1,7 @@
+from Solution import Distance
+
+def main():
+    print("matrix and maxs")
+    print("Answer is", Distance("s","matrix ","maxss"))
+
+main()
---
+++ Solution.py
@@ -0,0 +1,18 @@
+def Distance(self, word1, word2):
+    m,n = len(word1),len(word2)
+    ans = [[0 for i in range(n + 1)] for j in range(m + 1)]
+    for i in range(m + 1):
+        ans[i][n] = m - i
+    for i in range(n + 1):
+        ans[m][i] = n - i
+    m -= 1;n -= 1
+    while m >= 0:
+        t = n
+        while t >= 0:
+            if word1[m] == word2[t]:
+                ans[m][t] = ans[m + 1][t + 1]
+            else:
+                ans[m][t] = min(ans[m][t+1],ans[m+1][t],ans[m+1][t+1]) + 1
+            t -= 1
+        m -= 1
+    return ans[0][0]
